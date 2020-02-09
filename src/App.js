5c9d0c99a87fc37ab563935ae44205615b48a9a4
Ketones <Ketones@gmail.com>
2020-02-09 20:03:55 +0530

    routing functionality added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Route,Switch} from 'react-router-dom';
+
+
+const Hats = () => (
+
+<h1>Hats</h1>
+
+);
+
 function App() {
   return (
+  
     <div>
-    <HomePage />
+    <Switch>
+    <Route exact path='/' component={HomePage} />
+    <Route  path='/hats' component={Hats} />
+    </Switch>
     </div>
     
   );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,6 +3,9 @@ import './homepage.style.scss'
 
 import Directory from '../../components/directory/directory.component';
 
+
+
+
 const HomePage = () => (
     <div className='homepage'>
       
